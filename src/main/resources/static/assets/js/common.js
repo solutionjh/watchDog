9d5145d8cc9c90199d9da241d9ccf21ecc69de7a
jkj-mac <yaasanpig@andthebee.net>
2021-06-30 14:24:57 +0900

    1. 수행현황     - [x] 우선순위는 낮지만, 달력에서 앞뒤로만 움직이는걸 말고, 특정일자로 이동할 수 있는 방법     - [x] 시간까지 넣어서 적용 되도록 / 월달력에서는 시간이 빠지는게 좋지만, 안되면 어쩔수없고, 색상은 맞추기     - [x] 위험항목은 일자를 지우고 allDay가 되게끔, isAnomally처리 > 현재는 서버에서 안내려옴 처리는 해 둠     - [x] 위험(빨간색)인 항목을 위로 배치 해야함, > 서버에서 정렬 해 줌, 이건 우리가 데이터 바꿔서 테스트를 해 보고 문제가 되면 알려줘야함     - [x] 프로젝트(달력안, 테이블) 눌렀을 때 보고서로 이동으로

--- src/main/resources/static/assets/js/common.js
+++ src/main/resources/static/assets/js/common.js
@@ -368,3 +368,17 @@ function paramValidation(data ,target){
 	});	
 	return cnt  ==  0 ? false : true;
 }
+
+function formatDate(date) {
+    var d = new Date(date),
+        month = '' + (d.getMonth() + 1),
+        day = '' + d.getDate(),
+        year = d.getFullYear();
+
+    if (month.length < 2) 
+        month = '0' + month;
+    if (day.length < 2) 
+        day = '0' + day;
+
+    return [year, month, day].join('-');
+}
