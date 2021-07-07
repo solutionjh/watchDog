c37a5c45268b715d449810b2db3c2f605f4d4df8
eunwooyoung <kalli80@naver.com>
2021-07-07 15:04:01 +0900

    no message

--- src/main/java/com/nice/datafileanomalydetection/config/SecurityConfig.java
+++ src/main/java/com/nice/datafileanomalydetection/config/SecurityConfig.java
@@ -61,7 +61,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		
-		//http.csrf().disable(); //임시
+		//임시
+		/*
+		 * http.csrf().disable(); http.authorizeRequests() .anyRequest().permitAll();
+		 */
 		
 		setAntMatchers(http);		 
 		
