394f41e28593b8df798934baf219c6690ead3ff0
eunwooyoung <kalli80@naver.com>
2021-07-08 15:36:25 +0900

    csrf 제외 , 외부접속 url 설정 , 로그인 자동 설정

--- src/main/java/com/nice/datafileanomalydetection/config/SecurityConfig.java
+++ src/main/java/com/nice/datafileanomalydetection/config/SecurityConfig.java
@@ -62,9 +62,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		
 		//임시
-		/*
-		 * http.csrf().disable(); http.authorizeRequests() .anyRequest().permitAll();
-		 */
+		
+		http.csrf().disable(); 
+		http.authorizeRequests().antMatchers("/except/**").permitAll();
+		 
 		
 		setAntMatchers(http);		 
 		
